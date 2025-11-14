3a7c964a99775f066a5d912e6f37217f229020bb
Oleksandr445 <o.o.masiuk@student.khai.edu>
2025-11-14 09:28:35 +0200

    Add dinnerparty program finish

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -1,26 +1,28 @@
 import random
 
-# 1
-num_friends = int(input("Enter the number of friends joining (including you):\n"))
-
-if num_friends <= 0:
+# 1 + Захист
+num_input = input("Enter the number of friends joining (including you):\n> ")
+if not num_input.isdigit() or int(num_input) <= 0:
     print("No one is joining for the party")
 else:
+    num_friends = int(num_input)
     print("Enter the name of every friend (including you), each on a new line:")
     friends = {}
     for _ in range(num_friends):
-        name = input()
+        name = input("> ")
         friends[name] = 0
 
-    # 2
-    total_amount = int(input("Enter the total amount:\n"))
+    # 2 + Захист
+    total_input = input("Enter the total amount:\n> ")
+    while not total_input.isdigit():
+        total_input = input("Enter the total amount (numbers only):\n> ")
+    total_amount = int(total_input)
     share = round(total_amount / num_friends, 2)
     for friend in friends:
         friends[friend] = share
 
     # 3
-    answer = input('Do you want to use the "Who is lucky?" feature? Write Yes/No:\n')
-
+    answer = input('Do you want to use the "Who is lucky?" feature? Write Yes/No:\n> ')
     if answer == "Yes":
         lucky_one = random.choice(list(friends.keys()))
         print(f"{lucky_one} is the lucky one!")
@@ -32,7 +34,7 @@ else:
                 friends[friend] = 0
             else:
                 friends[friend] = new_share
+        print(friends)
     else:
         print("No one is going to be lucky")
-
-    print(friends)
+        print(friends)
